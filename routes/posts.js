51d05bfe332184e8fd78360bfb7dad9ae976db7b
sign0329@gmail.com <https://github.com/sign0329/tb-2023-10-25>
2023-11-25 18:09:53 +0900

    feet: 게시글 댓글기능 추가

--- routes/posts.js
+++ routes/posts.js
@@ -13,7 +13,12 @@ router.get('/insert', function(req, res){
 
 router.get('/read', function(req, res){
   const id=req.query.id;
-  res.render('index', {title:'게시글 정보', pageName:'posts/read.ejs', id})
+  res.render('index', {title:'게시글 정보', pageName:'posts/read.ejs', id :id})
+});
+
+router.get('/update', function(req, res){
+  const id=req.query.id;
+  res.render('index', {title:'게시글 수정', pageName:'posts/update.ejs', id:id})
 });
 
 module.exports = router;
